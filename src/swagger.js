41c1c25869db4bcf894908e3e400de11440a4b77
exornamb <exornamb@gmail.com>
2026-05-03 19:32:45 +0000

    add: swagger docs

--- src/swagger.js
+++ src/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://interim-assesment-exornamb.onrender.com/',
         description: 'Development server',
       },
     ],
